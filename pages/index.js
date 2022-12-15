08870dce7a8b5e06dfa2392c4f9e6f92143844c2
Joel Hart <joel.d.hart1@gmail.com>
2022-12-14 16:33:26 -0800

    added navbar with styling

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,25 @@ export default function Home() {
 
       <main>
         <section className="bg-slate-600 ">
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">HartCodes</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </div>
