eeeca6be12d289a2dfa345d93b91324d4c106d39
Joel Hart <joel.d.hart1@gmail.com>
2022-12-14 16:49:57 -0800

    added header section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import deved from "../public/dev-ed-wave.png";
+import code from "../public/code.png";
+import design from "../public/design.png";
+import consulting from "../public/consulting.png";
+import Image from "next/image";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div>
       <Head>
@@ -10,16 +22,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-slate-600 ">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+      <main className="bg-white px-10">
+        <section className="min-h-screen ">
+          <nav className="py-10 mb-12 flex justify-between ">
             <h1 className="font-burtons text-xl">HartCodes</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
-                />
+                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
               </li>
               <li>
                 <a
@@ -31,6 +40,26 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Joel Hart
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Developer and creator, community builder.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              Web Developer providing services for programming and design
+              content needs. Read about my work below!
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillYoutube />
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={deved} layout="fill" objectFit="cover" />
+            </div>
+          </div>
         </section>
       </main>
     </div>
