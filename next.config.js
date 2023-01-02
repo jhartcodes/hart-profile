1249bbbedaa95354808911100d1fbd717fc29706
Joel Hart <joel.d.hart1@gmail.com>
2023-01-02 14:53:34 -0800

    added webpack loader for video files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/hart-profile",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -133,7 +133,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">My Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I've made some really cool stuff.
+              I have made some really cool stuff.
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
