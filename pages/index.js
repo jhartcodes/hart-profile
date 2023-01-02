76383d505316e6f4bca968cb0f96be6209aa8145
Joel Hart <joel.d.hart1@gmail.com>
2023-01-02 14:31:51 -0800

    re organized projects

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+import web7 from "../public/web7.png";
 import video from "../public/video.mp4";
 import Link from "next/link.js";
 
@@ -90,7 +91,7 @@ export default function Home() {
               to help build a small
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I am currently looking for new opportunities.
+              I am open to new opportunities.
             </p>
           </div>
           <div className="lg:flex gap-10">
@@ -101,18 +102,13 @@ export default function Home() {
                 height={100}
                 className="mx-auto d-block"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creatied some beautiful front end applications.
-              </p>
-              <h4 className="py-4 text-teal-600">Projects </h4>
-              <p className="text-gray-800 py-1">Bloom</p>
-              <p className="text-gray-800 py-1">Trillo</p>
-              <p className="text-gray-800 py-1">Natours</p>
-              <p className="text-gray-800 py-1">Scheduler</p>
+              <h3 className="text-lg font-medium pt-8 pb-2  ">Projects</h3>
+              <p className="text-gray-800 py-2">Bloom</p>
+              <p className="text-gray-800 py-2">Trillo</p>
+              <p className="text-gray-800 py-2">Natours</p>
+              <p className="text-gray-800 py-2">Scheduler</p>
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image
                 src={code}
@@ -123,6 +119,7 @@ export default function Home() {
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Interested In working with me?
               </h3>
+
               <p className="py-2"> Form FORM </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
@@ -130,23 +127,13 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
-            {/* <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">Are you in need of a new website.</p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div> */}
           </div>
         </section>
         <section className="py-10">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">My Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Check out my work.
+              I've made some really cool stuff.
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
@@ -156,62 +143,52 @@ export default function Home() {
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web2}
-              />
+            <div class="card">
+              <h3 className="text-3xl my-10 dark:text-white ">Bloom</h3>
+              <div class="flex flex-wrap -mx-2">
+                <Image
+                  src={web2}
+                  width={50}
+                  height={50}
+                  class="w-1/2 px-2 mb-2"
+                />
+                <Image src={web1} class="w-1/2 px-2 mb-2" />
+                <Image src={web3} class="w-1/2 px-2 mb-2" />
+                <Image src={web4} class="w-1/2 px-2 mb-2" />
+              </div>
             </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web1}
-              />
+
+            <div class="card">
+              <h3 className="text-3xl my-10 dark:text-white ">Scheduler</h3>
+              <div class="flex flex-wrap -mx-2">
+                <Image
+                  src={web7}
+                  width={50}
+                  height={50}
+                  class="w-1/2 px-2 mb-2"
+                />
+                <Image
+                  src={web6}
+                  width={50}
+                  height={50}
+                  class="w-1/2 px-2 mb-2"
+                />
+              </div>
             </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                src={web6}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <video src="video/mp4" controls />
+            <div class="card">
+              <h3 className="text-3xl my-10 dark:text-white ">Trillo</h3>
+              <div class="flex flex-wrap -mx-2">
+                <Image
+                  src={web5}
+                  width={100}
+                  height={100}
+                  class="w-1/2 px-2 mb-2"
+                />
+              </div>
             </div>
           </div>
         </section>
-        <div>About Us</div>
+        <Link href="/projects">About Us</Link>
       </main>
     </div>
   );
